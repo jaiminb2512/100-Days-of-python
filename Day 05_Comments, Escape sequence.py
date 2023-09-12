c26c92103a8c438dabfa9cf0e9ae362cfcd18222
jaiminb2512 <137876433+jaiminb2512@users.noreply.github.com>
2023-09-12 23:28:50 +0530

    Update Day 05_Comments, Escape sequence.py

--- Day 05_Comments, Escape sequence.py
+++ Day 05_Comments, Escape sequence.py
@@ -1,5 +1,8 @@
 # By using '#' symbole you can comment in your code
 # If you want multiple comment in your code triple cote and triple double cote is used 
+""" My name is jaimin prajapati.
+    I am student of L.D. college of engineering 
+    This is my 5th day of 100 days of code"""
 
 # /n called escape sequence which use to print new line of output
 print("Hello jaimin\nHow are you?")
